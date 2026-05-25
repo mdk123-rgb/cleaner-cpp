7b0941ad476e982531cbe551f99122092f0ddc88
mdk123-rgb <domaradzkimateusz22@gmail.com>
2026-05-25 15:56:04 +0200

    zmoianyt

---
+++ src/src/backend/CMakeFiles/backend.dir/dummy.cpp.o.d
@@ -0,0 +1,2 @@
+src/backend/CMakeFiles/backend.dir/dummy.cpp.o: \
+ /home/matt/cleaner-cpp/src/backend/dummy.cpp /usr/include/stdc-predef.h
---
+++ src/src/frontend/CMakeFiles/SystemCleaner.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SystemCleaner.dir/link.d"
+  "CMakeFiles/SystemCleaner.dir/main.cpp.o"
+  "CMakeFiles/SystemCleaner.dir/main.cpp.o.d"
+  "CMakeFiles/SystemCleaner.dir/mainWindow.cpp.o"
+  "CMakeFiles/SystemCleaner.dir/mainWindow.cpp.o.d"
+  "SystemCleaner"
+  "SystemCleaner.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SystemCleaner.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
